da49eaf7fca34a89b130f105e660bc3f3fe96210
OnSebii <lang.sebastian.006@gmail.com>
2021-12-08 19:21:02 +0100

    Get Send Message

--- server/routes/index.js
+++ server/routes/index.js
@@ -40,4 +40,15 @@ router.post('/notify', (req) => {
   }
 });
 
+router.get('/send?', (req) => {
+  const payload = JSON.stringify({ title: 'New Message', body: req.query.msg });
+  for (const sub of subscription) {
+    try {
+      webpush.sendNotification(sub, payload);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+});
+
 module.exports = router;
